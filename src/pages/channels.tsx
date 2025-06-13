fd19dcc6dae3685d6ae54eeda65f5b37ff9de7ca
Gustavo Ribeiro <gugahribeiro@hotmail.com>
2025-06-13 14:55:40 -0300

    chore: add chat fallback

--- src/pages/channels.tsx
+++ src/pages/channels.tsx
@@ -1,8 +1,11 @@
 import { ChannelSidebar } from "@/components/channel-sidebar/channel-sidebar";
 import { ChannelMembersSidebar } from "@/components/channel-members-sidebar/channel-members-sidebar";
-import { Outlet } from "react-router";
+import { Outlet, useParams } from "react-router";
+import { Card, CardContent } from "@/components/ui/card";
+import { Hash } from "lucide-react";
 
 export function ChannelsPage() {
+  const { slug } = useParams()
 
   return (
     <div className="lg:flex gap-4">
@@ -11,7 +14,17 @@ export function ChannelsPage() {
         <ChannelMembersSidebar />
       </aside>
       <main className="flex-1 mt-4 lg:mt-0">
-        <Outlet />
+        {slug ? (
+          <Outlet />
+        ) : (
+          <Card className="flex justify-center items-center h-[600px]">
+            <CardContent className="text-center">
+              <Hash className="w-12 h-12 text-green-500 mx-auto mb-4" />
+              <h2 className="text-md lg:text-xl font-semibold text-foreground mb-2">Bem vindo(a) ao Canal de Comunicação da <span className="text-primary">Sorocaps</span></h2>
+              <p className="text-muted-foreground">Selecione um canal para começar a conversar!</p>
+            </CardContent>
+          </Card>
+        )}
       </main>
     </div >
   )
